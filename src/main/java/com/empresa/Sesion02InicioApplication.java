74975609d951608543f7cf242cc41552b686c4b7
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2021-08-22 00:16:09 -0500

    Inicio de sesión

---
+++ src/main/java/com/empresa/Sesion02InicioApplication.java
@@ -0,0 +1,13 @@
+package com.empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sesion02InicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sesion02InicioApplication.class, args);
+	}
+
+}
